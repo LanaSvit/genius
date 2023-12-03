aaa545548af5cd3bae18eb41b59b3d4b4f0cb01a
LanaSvit <antoniobespaleva@gmail.com>
2023-12-03 19:41:41 +0200

    swiper +adaptive layout for cards and footer

---
+++ js/formats-slider.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
